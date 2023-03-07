53433443870e8c774d895996c547a4811f223c1f
danielpark <danpark1@student.ubc.ca>
2023-03-07 00:33:59 -0800

    added gym class

--- src/test/model/GymTest.java
+++ src/test/model/GymTest.java
@@ -3,6 +3,8 @@ package model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class GymTest {
 
     private Gym gym1;
@@ -10,13 +12,25 @@ public class GymTest {
     @BeforeEach
     void runBefore() {
         gym1 = new Gym();
+
+
+    }
+
+
+    @Test
+    void addMemberTest() {
+
         Member mem1 = new Member("Daniel", 180, 80);
         Member mem2 = new Member("Paul", 170, 70);
 
+        gym1.addMember(mem1);
+        gym1.addMember(mem2);
+
+        assertEquals(2,gym1.getMemberList().size());
+        assertEquals(mem1, gym1.getMemberList().get(0));
+        assertEquals(mem2, gym1.getMemberList().get(1));
+
     }
 
-    @Test
-    void testConstructor() {
 
-    }
 }
--- src/test/model/ResultTest.java
+++ src/test/model/ResultTest.java
@@ -1,4 +1,28 @@
 package model;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ResultTest {
+
+    private Result result;
+
+    @BeforeEach
+    void runBefore() {
+
+        result = new Result(false, new Member("Daniel", 180 ,80));
+
+    }
+
+    @Test
+    void constructorTest() {
+
+
+        assertEquals(false,result.isTie());
+        assertEquals("Daniel", result.getMostDist().getName());
+        assertEquals(180, result.getMostDist().getHeight());
+        assertEquals(80, result.getMostDist().getWeight());
+    }
 }
