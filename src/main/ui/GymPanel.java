1eddb5b114d70e222261b826e464b3064f15ed4c
danielpark <danpark1@student.ubc.ca>
2023-04-13 18:45:04 -0700

    Phase 4: Task 2

--- src/main/ui/GymPanel.java
+++ src/main/ui/GymPanel.java
@@ -10,7 +10,6 @@ import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
 import java.util.Collections;
 import java.util.Iterator;
 
